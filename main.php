4ca152ea2bebe0cab7552e3cd47d44054a6c3812
Komeiji Koishi <bbleae@baka.studio>
2017-08-22 18:01:18 +0800

    不好意思刚才拿错了

--- main.php
+++ main.php
@@ -1,22 +1,10 @@
 <?php
+include('library/Requests.php'); //包含库
 ini_set("memory_limit","-1"); //取消内存限制(渣代码占内存太多)
 error_reporting(E_ALL); //参数为0则不输出报错信息, 输出全部报错信息请将参数改为 E_ALL
 
-include('library/Requests.php');
-include("library/ActiveRecord.php"); //包含库
-
-$dbms='mysql';     //数据库类型
-$host='127.0.0.1:3306'; //数据库主机名
-$dbName='draw';    //使用的数据库
-$user='bbleae';      //数据库连接用户名
-$pass='003800';          //对应的密码
-$dsn="$dbms:host=$host;dbname=$dbName";
-ActiveRecord::setDb(new PDO($dsn, $user, $pass, array(PDO::ATTR_PERSISTENT => true)));
-
-class user extends ActiveRecord {
-	public $table = 'user';
-	public $primaryKey = 'id';
-}
+/*获取Cookies*/
+$hs = json_decode(file_get_contents("cookies.json"),true)['cookies'];
 
 /*http请求Header模板*/
 $tpl = array(
@@ -30,38 +18,63 @@ $tpl = array(
     'Connection' => 'keep-alive',
 );
 
-//$mark = []; //标记该cookie在本次执行中是否被使用过
+$mark = []; //标记该cookie在本次执行中是否被使用过
 
+//$hs = ['cookie 1','cookie 2'......]; //$hs数组每一个元素存放一个账号的Cookie头
+
+/*将字符串分割成数组 by Komeiji Satori
+function mbStrSplit($string, $len = 1) {
+    $start = 0;
+    $strlen = mb_strlen($string);
+    while ($strlen) {
+        $array[] = mb_substr($string, $start, $len, "utf8");
+        $string = mb_substr($string, $len, $strlen, "utf8");
+        $strlen = mb_strlen($string);
+    }
+    return $array;
+}
+*/
+
+/*在绘板指定的坐标上色 x,y为整数 color为[0-9A-V]的一个字符*/
 function draw($x,$y,$color){
     Requests::register_autoloader();
+    global $hs;
     global $tpl;
     $url = 'https://api.live.bilibili.com/activity/v1/SummerDraw/draw';
-    $data = array('x_min' => $x,'y_min' => $y,'x_max' => $x,'y_max' => $y,'color' => $color);
-	$co_r = new user();
-	$co_r = $co_r->select('*')->lt('lastdraw',time()-180)->find();
-	//var_dump($co_r);
-	if($co_r){
-		$tpl['Cookie'] = $co_r->cookie;
-		$repo = Requests::post($url, $tpl, $data);
-		$co_r->lastdraw = time();
-		$co_r->update();
-		if (json_decode($repo->body)->code == 0) {
-			echo "\n".$co_r->id.": ".$repo->body.": $x, $y, $color";
-			unset($co_r);
-			return 0;
-		}else{
-			echo "\n".$co_r->id.": Error in one pixel: $x, $y, $color";
-			unset($co_r);
-			return 1;
-		}
-	}else{
-		unset($co_r);
-		return 2;
-	}
+    $data = array(
+        'x_min' => $x,
+        'y_min' => $y,
+        'x_max' => $x,
+        'y_max' => $y,
+        'color' => $color
+    );
+    $header_count = count($hs);
+    for ($i = 0;$i<$header_count;$i++) {
+        if (@$GLOBALS['mark'][$i] == 1)
+            continue;
+        else {
+            $tpl['Cookie'] = $hs[$i];
+            /*
+			$tmp_bitmap = json_decode(file_get_contents("https://api.live.bilibili.com/activity/v1/SummerDraw/bitmap"), true)['data']['bitmap'];
+			if ($tmp_bitmap[$y*1280+$x]==$color){
+				unset($tmp_bitmap);
+				return true;
+			}
+            */
+            $repo = Requests::post($url, $tpl, $data);
+            if (json_decode($repo->body, true)['code'] == 0) {
+                echo "\n[$i]: ".$repo->body.": $x, $y, $color";
+                $GLOBALS['mark'][$i] = 1;
+                unset($GLOBALS[$i]);
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 /*将一副图画到绘板上
-$p为一个hash map 键x,y,w,h对应的值均为整数 描述图像的坐标和高宽 键bitmap对应的值为一个字符串,描述了图像的内容 包含字符([0-9A-V]|Z), Z表示对该像素不做检测
+$p为一个hash map 键x,y,w,h对应的值均为整形 描述图像的坐标和高宽 键bitmap对应的值为一个字符串,描述了图像的内容 包含字符([0-9A-V]|Z), Z表示对该像素不做检测
 TODO: 目前只支持w=1280 h=720的图像
 bitmap示例:
 若bitmap为114514191981089364, w为6,h为3 则该图像为:
@@ -75,19 +88,12 @@ function exec_draw($p)
     $paint_str = json_decode(file_get_contents("https://api.live.bilibili.com/activity/v1/SummerDraw/bitmap"), true)['data']['bitmap'];
 
     for ($i=0;$i<921600;$i++){
-        if ($p['bitmap'][$i] == 'Z')
+        if ($p['bitmap'][$i] == 'Z'/*||$p['bitmap'][$i] == '1'*//*忽略白色*/)
             continue;
-        elseif ($paint_str[$i]!=$p['bitmap'][$i]) {
+        elseif ($p['bitmap'][$i] != $paint_str[$i]) {
             $c = $p['bitmap'][$i];
-			switch(draw($i%1280, floor($i/1280),$c)){
-				case 2:
-					die("\nSome pixel(s) left and colding down...");
-				case 1:
-				case 0:
-					break;
-				default:
-					echo "\nUncaugt error!";
-			}
+            if (!draw($i%1280, floor($i/1280), $c))
+                die("\nSome pixel(s) left and colding down...");
         }
     }
     die("\n All clear.");
@@ -118,4 +124,4 @@ function get_img_from_URL($url){
 }
 
 //从3Shain.me获取东方势力的像素画图纸并绘制
-exec_draw(get_img_from_URL('http://www.3Shain.me/data/all.json'));
+exec_draw(get_img_from_URL('http://www.3shain.me/data/all2.json'));
